337f7291e75f404e9c4a9b1e21232598de3c796c
Michele Mesiti <michele.mesiti@gmail.com>
2020-10-28 13:41:43 +0000

    some fixes and gitignore

--- flop_count_template.py.c
+++ flop_count_template.py.c
@@ -1,61 +1,62 @@
 #ifdef PYTHON
-#define ONELINE_FUNCDEF(name, body) def name(): return (body)
+// for one-line function definition
+#define _FD(name, body) def name(): return (body)
 #else
-#define ONELINE_FUNCDEF(name, body) static float name(){ return (body);}
+// for one-line function definition
+#define _FD(name, body) static float name(){ return (body);}
 #endif
 
-
-// see Dphi_flt.c
 
 #ifdef REPR_ADJOINT
-ONELINE_FUNCDEF(matrix_mul_flops, 4*NF*NF - 2*NF);
+_FD(matrix_mul_flops, 4*NF*NF - 2*NF);
 #else
-ONELINE_FUNCDEF(matrix_mul_flops, 8*NF*NF - 2*NF);
+_FD(matrix_mul_flops, 8*NF*NF - 2*NF);
 #endif
 
-ONELINE_FUNCDEF(real_op_flop , 1);
-ONELINE_FUNCDEF(complex_sum_flops , 2);
-ONELINE_FUNCDEF(complex_mul_flops , 6);
-ONELINE_FUNCDEF(complex_mulR_flops , 2); // capital R for legibility (hopefully);
-ONELINE_FUNCDEF(complex_norm_flops , 3); // only re ;
+_FD(real_op_flop , 1);
+_FD(complex_sum_flops , 2);
+_FD(complex_mul_flops , 6);
+_FD(complex_mulR_flops , 2); // capital R for legibility (hopefully);
+_FD(complex_norm_flops , 3); // only re ;
 
 // single vector
-ONELINE_FUNCDEF(vector_sum_flops , complex_sum_flops()*NF);
-ONELINE_FUNCDEF(vector_mul_flops , complex_mulR_flops()*NF); // mul uses mulR ;
-ONELINE_FUNCDEF(vector_norm_flops , complex_norm_flops()*NF+real_op_flop()*NF); // + NF sums (not NF-1)
+_FD(vector_sum_flops , complex_sum_flops()*NF);
+_FD(vector_mul_flops , complex_mulR_flops()*NF); // mul uses mulR ;
+_FD(vector_norm_flops , complex_norm_flops()*NF+real_op_flop()*NF); // + NF sums (not NF-1)
 
 // single spinor
-ONELINE_FUNCDEF(spinor_norm_flops , 4*vector_norm_flops() );
-ONELINE_FUNCDEF(spinor_sum_flops , 4*vector_sum_flops());
-ONELINE_FUNCDEF(spinor_mul_flops , 4*vector_mul_flops()); // uses mulR
-ONELINE_FUNCDEF(spinor_mul_add_assign_flops , spinor_mul_flops() + spinor_sum_flops());
+_FD(spinor_norm_flops , 4*vector_norm_flops() );
+_FD(spinor_sum_flops , 4*vector_sum_flops());
+_FD(spinor_mul_flops , 4*vector_mul_flops()); // uses mulR
+_FD(spinor_mul_add_assign_flops , spinor_mul_flops() + spinor_sum_flops());
 
-// spinor field related flop count
-ONELINE_FUNCDEF(site_spinor_field_sqnorm_f_flops , 1.0/2 * spinor_norm_flops()); // mult + add
-ONELINE_FUNCDEF(site_spinor_field_mul_add_assign_f_flops , 1.0/2 * spinor_mul_add_assign_flops());
-ONELINE_FUNCDEF(site_spinor_field_mul_f_flops , 1.0/2 * spinor_mul_flops());
-ONELINE_FUNCDEF(site_spinor_field_sub_f_flops , 1.0/2 * spinor_sum_flops());
-ONELINE_FUNCDEF(site_spinor_field_prod_re_flop , site_spinor_field_sqnorm_f_flops());
-ONELINE_FUNCDEF(site_spinor_field_add_assign_f_flops , 1.0/2 * spinor_sum_flops());
-ONELINE_FUNCDEF(site_spinor_field_minus_f_flops , 1.0/2 * spinor_mul_flops()); // as a mul
+// spinor field related flop count, for a single site
+_FD(site_spinor_field_sqnorm_f_flops , 1.0/2 * spinor_norm_flops()); // mult + add
+_FD(site_spinor_field_mul_add_assign_f_flops , 1.0/2 * spinor_mul_add_assign_flops());
+_FD(site_spinor_field_mul_f_flops , 1.0/2 * spinor_mul_flops());
+_FD(site_spinor_field_sub_f_flops , 1.0/2 * spinor_sum_flops());
+_FD(site_spinor_field_prod_re_flop , site_spinor_field_sqnorm_f_flops());
+_FD(site_spinor_field_add_assign_f_flops , 1.0/2 * spinor_sum_flops());
+_FD(site_spinor_field_minus_f_flops , 1.0/2 * spinor_mul_flops()); // as a mul
+
 
 // see Dphi.c
-ONELINE_FUNCDEF(site_Dphi_flops , 1.0/2*(16*matrix_mul_flops() +   \
+_FD(site_Dphi_flops , 1.0/2*(16*matrix_mul_flops() +   \
                                           45*vector_sum_flops()));
 
-ONELINE_FUNCDEF(site_Cphi_flops , 1.0/2*( 8*matrix_mul_flops() +   \
+_FD(site_Cphi_flops , 1.0/2*( 8*matrix_mul_flops() +   \
                                            4*vector_sum_flops() +   \
                                            spinor_mul_add_assign_flops()));
 
-ONELINE_FUNCDEF(site_Cphi_assign_flops , 1.0/2*( 8*matrix_mul_flops() + \
+_FD(site_Cphi_assign_flops , 1.0/2*( 8*matrix_mul_flops() + \
                                                   4*vector_sum_flops() + \
                                                   spinor_mul_add_assign_flops() + \
                                                   spinor_sum_flops()));
 
 
 
-ONELINE_FUNCDEF( N , 2*NF);
-ONELINE_FUNCDEF( forward_substitution_loop_flop , N()*(N()-1)/2 * ((3 * real_op_flop()) + // n = i*(i+1)/2+i) \
+_FD( N , 2*NF);
+_FD( forward_substitution_loop_flop , N()*(N()-1)/2 * ((3 * real_op_flop()) + // n = i*(i+1)/2+i) \
                                                      // _complex_mul_sub_assign(); )
                                                      (complex_mul_flops() + complex_sum_flops()) + \
                                                      // _complex_mul_sub_assign();)
@@ -98,10 +99,10 @@ float backward_substitution_loop_flop(){
 }
 #endif
 
-ONELINE_FUNCDEF( site_Cphi_inv_flops , 1.0/2*(forward_substitution_loop_flop() + \
+_FD( site_Cphi_inv_flops , 1.0/2*(forward_substitution_loop_flop() + \
                                       backward_substitution_loop_flop()));
 
-ONELINE_FUNCDEF( site_Cphi_inv_assign_flops , 1.0/2*(forward_substitution_loop_flop() + \
+_FD( site_Cphi_inv_assign_flops , 1.0/2*(forward_substitution_loop_flop() + \
                                              backward_substitution_loop_flop() + \
                                              spinor_sum_flops()));
 
@@ -134,7 +135,7 @@ float cg_out_of_loop_flops_per_site(float site_operator_flops){
            site_spinor_field_sub_f_flops() +
            site_spinor_field_sqnorm_f_flops() +
            site_operator_flops +
-           site_spinor_field_mul_add_assign_f_flops+
+           site_spinor_field_mul_add_assign_f_flops() +
            site_spinor_field_sub_f_flops() +
            2*site_spinor_field_sqnorm_f_flops());
 #ifndef PYTHON
